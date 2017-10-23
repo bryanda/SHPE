3b36068b960fff62b97f31f6111edc77fddea443
iij826 <xavid.ramirez01@utrgv.edu>
2017-10-22 23:09:25 -0500

    pushing new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// Include Pages here
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { ItineraryPage } from '../pages/itinerary/itinerary';
+import { EventsPage } from '../pages/events/events';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +24,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Itinerary', component: ItineraryPage },
+      { title: 'Events', component: EventsPage }
     ];
 
   }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,90 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  tab: string = "events";
+  Events = [
+    {
+      title: 'SHPE Conference Kickoff!!',
+      subtitle: 'ARE YOU READY?',
+      startdate: '10/25/2017',
+      endate: '',
+      startime: '0800',
+      endtime: '1000',
+      locationname: 'UTRGV Auditorium',
+      locationid: '121',
+      bannerURL: 'http://via.placeholder.com/280x80'
+    }, {
+      title: 'Company Introductions',
+      subtitle: 'See who came',
+      startdate: '10/25/2017',
+      endate: '',
+      startime: '0800',
+      endtime: '1000',
+      locationname: 'UTRGV Auditorium',
+      locationid: '121',
+      bannerURL: 'http://via.placeholder.com/280x80'
+    }, {
+      title: 'Workshops Intro',
+      subtitle: '',
+      startdate: '10/25/2017',
+      endate: '',
+      startime: '0800',
+      endtime: '1000',
+      locationname: 'UTRGV Auditorium',
+      locationid: '121',
+      bannerURL: 'http://via.placeholder.com/280x80'
+    }
+  ];
+  Schedule = [
+    {
+      title: 'Opening Cerimony',
+      time: '09:30',
+      locationname: 'South Hall'
+    },
+    {
+      title: 'Introduction to SVG',
+      time: '10:30',
+      locationname: 'East Auditorium 1.102'
+    },
+    {
+      title: 'Resume Builder',
+      time: '11:00',
+      locationname: 'ENGR 1.268'
+    },
+    {
+      title: 'Lunch Time',
+      time: '12:00',
+      locationname: 'Student Union'
+    },
+    {
+      title: 'Virtual Reality Talk',
+      time: '13:30',
+      locationname: 'EDUC 1.285'
+    },
+  ];
+  Companies = [
+    {
+      name: 'America Airlines',
+      email: 'changeme@email.com',
+      logo: '../../assets/imgs/logo.png'
+    }, {
+      name: 'Chik-fil-a',
+      email: 'changeme@email.com',
+      logo: '../../assets/imgs/logo.png'
+    }, {
+      name: 'Taco Bell',
+      email: 'changeme@email.com',
+      logo: '../../assets/imgs/logo.png'
+    }, {
+      name: 'NASA',
+      email: 'changeme@email.com',
+      logo: '../../assets/imgs/logo.png'
+    }, {
+      name: 'Space-X',
+      email: 'changeme@email.com',
+      logo: '../../assets/imgs/logo.png'
+    }
+  ];
   constructor(public navCtrl: NavController) {
 
   }
